49e3cb58719bb78cd231410225fa99baa2b05af5
Jorge Rodarte <ja.rodartemartinez@gmail.com>
2016-06-19 12:23:43 -0500

    v0.4

--- behavior/comportamiento_js.js
+++ behavior/comportamiento_js.js
@@ -1,4 +1,4 @@
-function cambio_tela(){
+function cambio_tela() {
 	alto=document.getElementById("alto_m").value;
 	ancho=document.getElementById("ancho_m").value;
 	x=document.getElementById("valor_x").value;
@@ -6,52 +6,88 @@ function cambio_tela(){
 	document.getElementById("precio_mosquitero").innerHTML=precio;
 }
 
-function MF_clasico(){
+function M_fijo(){
 	alto=document.getElementById("alto_MF").value;
 	ancho=document.getElementById("ancho_MF").value;
-	x=document.getElementById("valor_1").value;
-	precio=alto*ancho*x;
-	document.getElementById("pre_clasico").innerHTML=precio;
+
+	clasico=document.getElementById("valor_1").value;
+	precio_clasico=alto*ancho*clasico;
+	document.getElementById("pre_clasico").innerHTML=precio_clasico;
+
+    color=document.getElementById("valor_2").value;
+	precio_color=alto*ancho*color;
+	document.getElementById("pre_color").innerHTML=precio_color;
 }
 
-function MF_color(){
-	alto=document.getElementById("alto_MF").value;
-	ancho=document.getElementById("ancho_MF").value;
-	x=document.getElementById("valor_2").value;
-	precio=alto*ancho*x;
-	document.getElementById("pre_color").innerHTML=precio;
-}
-
-function MC_Vertical(x){
+function M_corredizo(){
     alto=document.getElementById("alto_MC").value;
-    costo=(x*2)*alto;
-    document.getElementById("costo_vertical_basico").innerHTML=costo;
-}
-
-function MC_Horizontal(y){
     ancho=document.getElementById("ancho_MC").value;
-    costo=(y*2)*ancho;
-    document.getElementById("costo_horizontal_basico").innerHTML=costo;
-}
+    c_vertical_b=60;
+    c_horizontal_b=46;
+    c_adaptador_b=25;
+    c_angulo_b=20;
+    c_vertical_c=70;
+    c_horizontal_c=55;
+    c_adaptador_c=28.5;
+    c_angulo_c=22;
+    c_jal=7;
+    c_carret=7;
+    c_vinil=2;
+    c_tela=24;
+    c_tornillo=1.2;
+    c_silicon=30;
 
-function MC_Adaptador(a){
-    ancho=document.getElementById("ancho_MC").value;
-    costo=(a*2)*ancho;
-    document.getElementById("costo_adaptador_basico").innerHTML=costo;
-}
 
-function MC_Angulo(a){
-    ancho=document.getElementById("ancho_MC").value;
-    costo=(a*2)*ancho;
-    document.getElementById("costo_angulo_basico").innerHTML=costo;
-}
+    vert_b=(c_vertical_b*2)*alto;
+    document.getElementById("costo_vertical_basico").innerHTML=vert_b;
+    vert_c=(c_vertical_c*2)*alto;
+    document.getElementById("costo_vertical_color").innerHTML=vert_c;
 
-function MC_jal(j){
-    costo=(j*2);
-    document.getElementById("costo_jal_basico").innerHTML=costo;
-}
+    hori_b=(c_horizontal_b*2)*ancho;
+    document.getElementById("costo_horizontal_basico").innerHTML=hori_b;
+    hori_c=(c_horizontal_c*2)*ancho;
+    document.getElementById("costo_horizontal_color").innerHTML=hori_c;
 
-function MC_Carret(c){
-    costo=(c*2);
-    document.getElementById("costo_carret_basico").innerHTML=costo;
+    adap_b=(c_adaptador_b*2)*ancho;
+    document.getElementById("costo_adaptador_basico").innerHTML=adap_b;
+    adap_c=(c_adaptador_c*2)*ancho;
+    document.getElementById("costo_adaptador_color").innerHTML=adap_c;
+
+    angu_b=(c_angulo_b*2)*ancho;
+    document.getElementById("costo_angulo_basico").innerHTML=angu_b;
+    angu_c=(c_angulo_c*2)*ancho;
+    document.getElementById("costo_angulo_color").innerHTML=angu_c;
+
+    jal_b_c=(c_jal*2);
+    document.getElementById("costo_jal_basico").innerHTML=jal_b_c;
+    document.getElementById("costo_jal_color").innerHTML=jal_b_c;
+
+    carr_b_c=(c_carret*2);
+    document.getElementById("costo_carret_basico").innerHTML=carr_b_c;
+    document.getElementById("costo_carret_color").innerHTML=carr_b_c;
+
+    vinil_b_c=((alto*c_vinil)+(ancho*c_vinil));
+    document.getElementById("costo_vinil_basico").innerHTML=vinil_b_c;
+    document.getElementById("costo_vinil_color").innerHTML=vinil_b_c;
+
+    tela_b_c=(alto*ancho*c_tela);
+    document.getElementById("costo_tela_basico").innerHTML=tela_b_c;
+    document.getElementById("costo_tela_color").innerHTML=tela_b_c;
+
+    torni_b_c=(c_tornillo*4);
+    document.getElementById("costo_tornillo_basico").innerHTML=torni_b_c;
+    document.getElementById("costo_tornillo_color").innerHTML=torni_b_c;
+
+    document.getElementById("costo_silicon_basico").innerHTML=c_silicon;
+    document.getElementById("costo_silicon_color").innerHTML=c_silicon;
+
+    suma_total_b=vert_b+hori_b+adap_b+angu_b+jal_b_c+carr_b_c+vinil_b_c+tela_b_c+torni_b_c+c_silicon;
+    document.getElementById("suma_total_basico").innerHTML=suma_total_b;
+    suma_total_c=vert_c+hori_c+adap_c+angu_c+jal_b_c+carr_b_c+vinil_b_c+tela_b_c+torni_b_c+c_silicon;
+    document.getElementById("suma_total_color").innerHTML=suma_total_c;
+
+    costo_total_b=(alto*ancho*520);
+    document.getElementById("costo_total_basico").innerHTML=costo_total_b;
+    costo_total_c=(alto*ancho*450);
+    document.getElementById("costo_total_color").innerHTML=costo_total_c;
 }
--- view/mosquiteros.php
+++ view/mosquiteros.php
@@ -13,12 +13,14 @@
 		<td>Alto</td>
 		<td>Ancho</td>
 		<td>X</td>
+        <td></td>
 		<td>PRECIO</td>
 	</tr>
 	<tr>
 		<td><input type="text" name="alto_moquitero" id="alto_m"></td>
 		<td><input type="text" name="ancho_mosquitero" id="ancho_m"></td>
 		<td><input type="text" name="valor_x" id="valor_x" value="150" placeholder="150"></td>
+        <td><button onclick="cambio_tela()">Calcular</button></td>
 		<td><div class="precios" id="precio_mosquitero" onmouseover="cambio_tela()">-</div></td>
 	</tr>
 </table>
@@ -32,18 +34,20 @@
 	<tr>
 		<td>Alto</td>
 		<td>Ancho</td>
-		<td>X1</td>
-		<td>Clasico</td>
-		<td>X2</td>
-		<td>Color</td>
+		<td>clasico</td>
+        <td>color</td>
+        <td></td>
+		<td>Precio clasico</td>
+		<td>Precio color</td>
 	</tr>
 	<tr>
 		<td><input type="text" name="alto_m" id="alto_MF"></td>
 		<td><input type="text" name="ancho_m" id="ancho_MF"></td>
 		<td><input type="text" name="X1" id="valor_1" value="300" placeholder="300"></td>
-		<td><div class="precios" id="pre_clasico" onmouseover="MF_clasico()">-</div></td>
-		<td><input type="text" name="X2" id="valor_2" value="350" placeholder="350"></td>
-		<td><div class="precios" id="pre_color" onmouseover="MF_color()">-</div></td>
+        <td><input type="text" name="X2" id="valor_2" value="350" placeholder="350"></td>
+        <td><button onclick="M_fijo()">Calcular</button></td>
+		<td><div class="precios" id="pre_clasico" onmouseover="M_fijo()">-</div></td>
+		<td><div class="precios" id="pre_color" onmouseover="M_fijo()">-</div></td>
 	</tr>
 </table>
 <br>
@@ -56,6 +60,7 @@
 	<tr>
 		<td colspan="2">Alto: <input type="text" name="alto_MC" id="alto_MC"></td>
 		<td colspan="2">Ancho: <input type="text" name="ancho_MC" id="ancho_MC"></td>
+        <td colspan="2"><button onclick="M_corredizo()">Calcular</button></td>
 	</tr>
 	<tr>
 		<th>Vertical</th>
@@ -72,16 +77,32 @@
 		<th>ITEM 1</th>
 	</tr>
     <tr>
-        <td><div id="costo_vertical_basico"  onmouseover="MC_Vertical(60)">-</div></td>
-        <td><div id="costo_horizontal_basico" onmouseover="MC_Horizontal(46)">-</div></td>
-        <td><div id="costo_adaptador_basico" onmouseover="MC_Adaptador(25)">-</div></td>
-        <td><div id="costo_angulo_basico" onmouseover="MC_Angulo(20)">-</div></td>
-        <td><div id="costo_jal_basico" onmouseover="MC_jal(7)">-</div></td>
-        <td><div id="costo_carret_basico" onmouseover="MC_Carret(7)">-</div></td>
+        <td><div id="costo_vertical_basico">-</div></td>
+        <td><div id="costo_horizontal_basico">-</div></td>
+        <td><div id="costo_adaptador_basico">-</div></td>
+        <td><div id="costo_angulo_basico">-</div></td>
+        <td><div id="costo_jal_basico">-</div></td>
+        <td><div id="costo_carret_basico">-</div></td>
         <td><div id="costo_vinil_basico">-</div></td>
         <td><div id="costo_tela_basico">-</div></td>
         <td><div id="costo_tornillo_basico">-</div></td>
         <td><div id="costo_silicon_basico">-</div></td>
+        <td><div id="suma_total_basico">-</div></td>
+        <td><div id="costo_total_basico">-</div></td>
+    </tr>
+    <tr>
+        <td><div id="costo_vertical_color">-</div></td>
+        <td><div id="costo_horizontal_color">-</div></td>
+        <td><div id="costo_adaptador_color">-</div></td>
+        <td><div id="costo_angulo_color">-</div></td>
+        <td><div id="costo_jal_color">-</div></td>
+        <td><div id="costo_carret_color">-</div></td>
+        <td><div id="costo_vinil_color">-</div></td>
+        <td><div id="costo_tela_color">-</div></td>
+        <td><div id="costo_tornillo_color">-</div></td>
+        <td><div id="costo_silicon_color">-</div></td>
+        <td><div id="suma_total_color">-</div></td>
+        <td><div id="costo_total_color">-</div></td>
     </tr>
 </table>
 </body>
