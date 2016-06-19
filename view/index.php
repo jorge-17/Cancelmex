fb3b1ce664a549c79452ef38d3e627f164463d27
Jorge Rodarte <ja.rodartemartinez@gmail.com>
2016-06-19 14:23:45 -0500

    v0.5

---
+++ view/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Index</title>
+        <link rel="stylesheet" type="text/css" href="../style/bootstrap-3.3.6-dist/css/bootstrap.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <a class="navbar-brand">Cancelmex</a>
+                <ul class="nav navbar-nav">
+                    <li>
+                        <a href="mosquiteros.php">Mosquiteros</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </body>
+</html>
